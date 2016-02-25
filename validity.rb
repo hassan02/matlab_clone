0cf7a3f92be368be071884f2db55c4a1c6d041c8
Hassan <oyeboadehassan@gmail.com>
2016-02-25 15:17:21 +0100

    Validity for Matlab Clone

--- validity.rb
+++ validity.rb
@@ -1,11 +1,30 @@
 class Valid
   # Class to check validity of string
   def inv2_2(mat)
+    n = mat.split(';')[0].split(' ').length
+    a = mat.split(';')[0].split(' ')[0].to_i
+    b = mat.split(';')[0].split(' ')[1].to_i
+    c = mat.split(';')[1].split(' ')[0].to_i
+    d = mat.split(';')[1].split(' ')[1].to_i
+    det = (a * d) - (b * c)
+    puts "#{a / det.to_f}\t#{-1 * c / det.to_f}\n#{-1 * b / det.to_f}\t#{d / det.to_f}"
       end
-
   def inv3_3(mat)
   end
-
+  def getelement(mat)
+    a = mat.split(';')[0].split(' ')[0].to_i
+    b = mat.split(';')[0].split(' ')[1].to_i
+    c = mat.split(';')[0].split(' ')[2].to_i
+    d = mat.split(';')[0].split(' ')[0].to_i
+    e = mat.split(';')[0].split(' ')[1].to_i
+    f = mat.split(';')[0].split(' ')[2].to_i
+    g = mat.split(';')[0].split(' ')[0].to_i
+    h = mat.split(';')[0].split(' ')[1].to_i
+    i = mat.split(';')[0].split(' ')[2].to_i
+  end
+  
+  def get determinant(mat)
+  end
   def check_array(array)
     if /^\d+(\s\d+)*$/ =~ array
       true
@@ -58,15 +77,27 @@ class Valid
     end
   end
 
-  def hor_concat(mata, matb, _operator)
-    @matamatb = ''
+  def ver_concat(mata, matb, _operator)
+    @verconcat = ''
     mata.split(';').each do |x|
-      @matamatb << "#{x}\n"
+      @verconcat << "#{x}\n"
     end
     matb.split(';').each do |y|
-      @matamatb << "#{y}\n"
+      @verconcat << "#{y}\n"
     end
+    puts '[a;b] = '.yellow
+    puts @verconcat.to_s.yellow
+  end
+
+  def hor_concat(mata, matb, _operator)
+    @horconcat = ''
+    rowa = mata.split(';').length
+    rowb = matb.split(';').length
+    for i in 0..rowa - 1
+      newmat = ''
+      newmat << mata.split(';')[i] << ' ' << matb.split(';')[i]
+      @horconcat << "#{newmat}\n".yellow
     end
-  puts '[a;b] = '.yellow
-  puts @matamatb.to_s.yellow
+    puts @horconcat
+end
 end
